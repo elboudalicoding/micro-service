4b1b51a7d739d12e0869812d17a73fb9b2c34de0
ossama <ossamaelboudali56@gmail.com>
2025-04-14 11:24:49 +0100

    The communication has been done successfully using Spring cloud OpenFeign ,postman request

--- order/src/main/java/org/micro/order/OrderController.java
+++ order/src/main/java/org/micro/order/OrderController.java
@@ -4,48 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
-
 @RestController
-@RequestMapping("/api/v1/orders")
+@RequestMapping("/api/orders")
 public class OrderController {
 
     @Autowired
     private OrderService orderService;
 
-    @PostMapping
-    public ResponseEntity<Order> createOrder(@RequestBody OrderRequest orderRequest) {
-        Order newOrder = orderService.createOrder(orderRequest);
-        return ResponseEntity.status(201).body(newOrder);
-    }
-
-    @GetMapping
-    public ResponseEntity<List<Order>> getAllOrders() {
-        List<Order> orders = orderService.getAllOrders();
-        return ResponseEntity.ok(orders);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Order> getOrderById(@PathVariable Integer id) {
-        Optional<Order> order = orderService.getOrderById(id);
-        return order.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Order> updateOrder(@PathVariable Integer id, @RequestBody OrderRequest orderRequest) {
-        Optional<Order> updatedOrder = orderService.updateOrder(id, orderRequest);
-        return updatedOrder.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteOrder(@PathVariable Integer id) {
-        if (orderService.getOrderById(id).isPresent()) {
-            orderService.deleteOrder(id);
-            return ResponseEntity.noContent().build();
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    @GetMapping("/test/product/{id}")
+    public ResponseEntity<String> testProductCommunication(@PathVariable("id") Long id) {
+        String response = orderService.testProductServiceCommunication(id);
+        return ResponseEntity.ok(response);
     }
 }
-
